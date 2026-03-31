aedd04d6789104dfa8ed88c06b5c75e0fc636cdc
Ashik <ashik.ar@atwic.com>
2026-03-31 17:15:20 +0530

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Kemi-Collection/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
